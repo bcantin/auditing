f5634b0ccfc8396cd974857e991aae7500157594
Brad Cantin <bradc@sunbelt-software.com>
2011-03-25 13:36:27 -0400

    removed whitespace.  travisci test

--- spec/schema.rb
+++ spec/schema.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Schema.define(:version => 0) do
-  
+
   create_table :audits do |t|
     t.string  :action
     t.string  :auditable_type
@@ -12,30 +12,30 @@ ActiveRecord::Schema.define(:version => 0) do
     t.integer :user_id
     t.boolean :undoable, :default => true
     t.timestamps
-  end  
-  
+  end
+
   create_table :schools do |t|
     t.string   :name
     t.datetime :established_on
     t.timestamps
   end
-  
+
   create_table :cars do |t|
     t.string  :name
     t.integer :auto_maker_id
     t.timestamps
   end
-  
+
   create_table :auto_makers do |t|
     t.string :name
     t.timestamps
   end
-  
+
   create_table :companies do |t|
     t.string :name
     t.timestamps
   end
-  
+
   create_table :phone_numbers do |t|
     t.string  :number
     t.string  :extension
@@ -43,22 +43,22 @@ ActiveRecord::Schema.define(:version => 0) do
     t.integer :phoneable_id
     t.timestamps
   end
-  
+
   create_table :people do |t|
     t.string :first_name
     t.string :last_name
     t.timestamps
   end
-  
+
   create_table :employments do |t|
     t.integer  :person_id
     t.integer  :company_id
     t.string   :start_date
     t.timestamps
   end
-  
+
   create_table :users do |t|
     t.string :email
   end
-  
+
 end
